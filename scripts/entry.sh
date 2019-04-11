adcde868def078380a1e41b4365ada114486c339
xw5d <xw5d@vpn-user-1-120.itc.virginia.edu>
2019-04-11 11:29:07 -0400

    Add migration command in the startup script and prepopulate data to vivas table

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,5 +1,9 @@
 # remove stale pid files
 rm -f $APP_HOME/tmp/pids/server.pid > /dev/null 2>&1
 
+# run database migration
+rake db:migrate
+rails db:seed
+
 # run the server
 rails server -b 0.0.0.0 -p 3000
